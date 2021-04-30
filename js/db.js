4d864922bf9269b77fb3ce5338361a8bd7d9b171
formosatto <formosatto@gmail.com>
2021-04-30 22:28:59 +0800

    Refacter all

---
+++ js/db.js
@@ -0,0 +1,24 @@
+function wait(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
+class DB {
+
+    constructor(name){
+        this.name = name;
+        this.flag = true;
+        this.url = '/src/db/' + name + '.json';
+    }
+
+    async query() {
+        try{
+            const response = await fetch(this.url);
+            await wait(1000);
+            const json = await response.json();
+            return json;
+        }catch (err) {
+            console.log('fetch failed: ' + err);
+        }
+    }
+
+}
